c2b3bff71a8e9f1e251dafc53de8f4f823e3bfbe
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-02-13 18:57:49 +0200

    Подготовка к созданию множества оружий

--- Assets/Scripts/Entity/Player/Player.cs
+++ Assets/Scripts/Entity/Player/Player.cs
@@ -3,19 +3,6 @@ using UnityEngine;
 public class Player : Entity
 {
     [SerializeField] private Input _input;
-    [SerializeField] private PlayerShooting _shooting;
-
-    private void Start()
-    {
-        _input.controls.Player.Shoot.performed += _shooting.TakeInput;
-        _input.controls.Player.Shoot.canceled += _shooting.TakeInput;
-    }
-
-    private void OnDisable()
-    {
-        _input.controls.Player.Shoot.performed -= _shooting.TakeInput;
-        _input.controls.Player.Shoot.canceled -= _shooting.TakeInput;
-    }
 
     private void FixedUpdate()
     {
